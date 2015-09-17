7c44b0696317ea5add695ccd78c893ba5045558d
Lekshmi Raveendranathapanicker <lekshmi.ravindranath@gmail.com>
2015-09-16 18:54:09 -0700

    Initial Commit

---
+++ BabyNoggin-DemoTests/BabyNoggin_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  BabyNoggin_DemoTests.swift
+//  BabyNoggin-DemoTests
+//
+//  Created by Deepu Mukundan on 9/16/15.
+//  Copyright (c) 2015 Lekshmi Raveendranathapanicker. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class BabyNoggin_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
